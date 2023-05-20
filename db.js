9377e9e4ea31532cdbb32e832dc340d3a50651e8
s222309556 <s222309556@deakin.edu.au>
2023-05-20 22:15:52 +1000

    test

---
+++ db.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const uri = 'mongodb+srv://admin:admin@cluster0.3lflt16.mongodb.net/?retryWrites=true&w=majority';
+
+const db = async ()=> {
+    try {
+        const con = awai
+    }
+
+    //w
+}
