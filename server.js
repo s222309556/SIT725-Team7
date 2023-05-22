8ade04fd0adadeb5382e07fce9b2f248e5a34dba
s222309556 <s222309556@deakin.edu.au>
2023-05-23 01:29:28 +1000

    make it better

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 //const port = 3000;
+//test code
 const port = process.env.PORT || 3000; //port changed for heroku
 const db = require("./db");
 
